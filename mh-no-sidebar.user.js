459c751301f84c274f7f3336dcbe0dfc31d6c114
Brad Parbs <brad@bradparbs.com>
2023-08-08 19:47:25 -0500

    Update to 1.0.4

--- mh-no-sidebar.user.js
+++ mh-no-sidebar.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         🐭️ MouseHunt - No Sidebar
-// @version      1.0.3
+// @version      1.0.4
 // @description  Hides the sidebar and moves it into a tab in the top menu.
 // @license      MIT
 // @author       bradp
@@ -15,7 +15,6 @@
 ((function () {
   'use strict';
 
-  // Move sidebar into menu tab.
   const moveSidebar = () => {
     // Create menu tab.
     const menuTab = document.createElement('div');
@@ -75,33 +74,43 @@
     tabsContainer.insertBefore(menuTab, tabsContainer.lastChild);
   };
 
-  addStyles(`body .pageFrameView {
+  const addBodyClass = () => {
+    const body = document.querySelector('body');
+    if (! body) {
+      return;
+    }
+
+    body.classList.add('no-sidebar');
+  };
+
+  addStyles(`/* Reflow grid for no sidebar */
+  body.no-sidebar .pageFrameView {
     -ms-grid-columns: [first] auto [content-start] 760px [content-end] auto [last];
     grid-template-columns: [first] auto [content-start] 760px [content-end] auto [last];
   }
 
   /* Sidebar moved into tab */
-  .pageFrameView .pageSidebarView-user {
+  body.no-sidebar .pageFrameView .pageSidebarView-user {
     padding: 0 0 10px;
     border-bottom: none;
   }
 
-  .pageSidebarView {
+  body.no-sidebar .pageSidebarView {
     display: none;
   }
 
-  .mousehuntHeaderView .menuItem.sidebar.dropdown {
+  body.no-sidebar .mousehuntHeaderView .menuItem.sidebar.dropdown {
     cursor: unset;
   }
 
-  .mousehuntHeaderView .menuItem.sidebar .dropdownContent {
+  body.no-sidebar .mousehuntHeaderView .menuItem.sidebar .dropdownContent {
     width: 365px;
 
     /* 10px padding + 180px scoreboard + 5px gap + 180px scoreboard + 10px padding */
     padding: 10px;
   }
 
-  .mousehuntHeaderView .menuItem.sidebar .dropdownContent a {
+  body.no-sidebar .mousehuntHeaderView .menuItem.sidebar .dropdownContent a {
     display: unset;
     height: auto;
     padding: 0;
@@ -109,14 +118,14 @@
     border-bottom: none;
   }
 
-  .mousehuntHeaderView .menuItem.sidebar .dropdownContent a:hover,
-  .mousehuntHeaderView .menuItem.sidebar .dropdownContent a:focus {
+  body.no-sidebar .mousehuntHeaderView .menuItem.sidebar .dropdownContent a:hover,
+  body.no-sidebar .mousehuntHeaderView .menuItem.sidebar .dropdownContent a:focus {
     text-decoration: underline;
     background-color: unset;
   }
 
   /* Image */
-  .mousehuntHeaderView .menuItem.sidebar .dropdownContent a.pageSidebarView-user-image {
+  body.no-sidebar .mousehuntHeaderView .menuItem.sidebar .dropdownContent a.pageSidebarView-user-image {
     width: 30px;
     height: 30px;
     padding: 0;
@@ -127,7 +136,7 @@
   }
 
   /* Name */
-  .mousehuntHeaderView .menuItem.sidebar .dropdownContent .pageSidebarView-user a:nth-child(2) {
+  body.no-sidebar .mousehuntHeaderView .menuItem.sidebar .dropdownContent .pageSidebarView-user a:nth-child(2) {
     display: inline;
     padding: 0;
     font-size: inherit;
@@ -137,16 +146,17 @@
   }
 
   /* <br> between name and logout */
-  .mousehuntHeaderView .menuItem.sidebar .dropdownContent .pageSidebarView-user br {
+  body.no-sidebar .mousehuntHeaderView .menuItem.sidebar .dropdownContent .pageSidebarView-user br {
     display: none;
   }
 
   /* Logout */
-  .mousehuntHeaderView .menuItem.sidebar .dropdownContent a.pageSidebarView-user-logout {
+  body.no-sidebar .mousehuntHeaderView .menuItem.sidebar .dropdownContent a.pageSidebarView-user-logout {
     display: inline-block;
     float: right;
     height: auto;
     padding: 5px 0;
+    margin-right: 10px;
     font-size: inherit;
     font-variant: none;
     color: #3b5998;
@@ -154,19 +164,19 @@
     border-radius: 0;
   }
 
-  .scoreboardRankingsWrapper {
+  body.no-sidebar .scoreboardRankingsWrapper {
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-gap: 5px;
     line-height: 14px;
   }
 
-  .scoreboardRelativeRankingTableView-table {
+  body.no-sidebar .scoreboardRelativeRankingTableView-table {
     padding-top: 5px;
     background: #fff;
   }
 
-  .mousehuntHeaderView .menuItem.sidebar .dropdownContent .scoreboardRankingsWrapper a {
+  body.no-sidebar .mousehuntHeaderView .menuItem.sidebar .dropdownContent .scoreboardRankingsWrapper a {
     font-size: 9px;
     color: #3b5998;
     text-decoration: none;
@@ -174,11 +184,15 @@
     border-radius: 0;
   }
 
-  .mousehuntHeaderView .menuItem.sidebar .dropdownContent .scoreboardRankingsWrapper a:hover,
-  .mousehuntHeaderView .menuItem.sidebar .dropdownContent .scoreboardRankingsWrapper a:focus {
+  body.no-sidebar .mousehuntHeaderView .menuItem.sidebar .dropdownContent .scoreboardRankingsWrapper a:hover,
+  body.no-sidebar .mousehuntHeaderView .menuItem.sidebar .dropdownContent .scoreboardRankingsWrapper a:focus {
     text-decoration: underline;
-  }
-  `);
+  }`);
 
+  addBodyClass();
+  onPageChange({ camp: { show: addBodyClass } });
+  onTravel(null, { callback: () => {
+    setTimeout(addBodyClass, 500);
+  } });
   moveSidebar();
 })());
